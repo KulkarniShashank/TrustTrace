60759583a0b60c63f5265fb5c7665152a273e112
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-05-15 12:35:21 +0530

    added the server in swagger

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .addServer(`http://localhost:3005`)
+    .addServer(`https://trusttrace-api.credebl.id`)
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
